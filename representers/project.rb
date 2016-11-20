5ef98ba7f419d4eaadc4bfb80313234e47ce85b3
wtlin1228 <wtlin0711@gmail.com>
2016-11-19 23:22:34 -0800

    representer for project

---
+++ representers/project.rb
@@ -0,0 +1,27 @@
+require 'roar/decorator'
+require 'roar/json'
+
+# Uaage : p1 = new Project.new(5, 1, 'Hello', 'wtlin1228')
+#         project_in_json = ProjectRepresenter.new(p1).to_json
+
+class ProjectRepresenter < Roar::Decorator
+	include Roar::JSON
+
+	property :userId
+    property :projectName
+    property :dateStart
+    property :dateEnd
+
+end
+
+
+# class Project
+# 	attr_reader :userId, :projectName, :dateStart, :dateEnd
+
+# 	def initialize(userId, projectName, dateStart, dateEnd)
+# 		@userId = userId
+# 		@projectName = projectName
+# 		@dateStart = dateStart
+# 		@dateEnd = dateEnd
+# 	end
+# end
