ef584f70c6786d0dbb4a0cca392db0935961ae72
wtlin1228 <wtlin0711@gmail.com>
2016-11-26 23:50:42 -0800

    get room info

--- controllers/rooms.rb
+++ controllers/rooms.rb
@@ -6,14 +6,28 @@ class TimeTravelerAPI < Sinatra::Base
     location = params[:location]
     begin
       rent = Airbnb::RentInfo.find(location: location)
+
+      # Get each room's info
+      rent.infos.each do |room|
+        room_id = room.id
+        room_name = room.name
+        room_address = room.address
+        room_airbnb_link = room.airbnb_link
+        room_roomImg = room.roomImg
+        room_bed = room.bed
+        room_roomRank = room.roomRank
+      end
+
+      # Shown on the broswer
       content_type 'application/json'
       {
         location: rent.location,
-        infos: rent.infos
+        infos: rooms_info
       }.to_json
 
-      rescue
-        halt 404, "Cannot find the location"
-      end
+
+    rescue
+      halt 404, "Cannot find the location"
+    end
   end
 end
