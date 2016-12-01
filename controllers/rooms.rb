c2f2272fad53bee7b134cf37295d86b1478d852b
wtlin1228 <wtlin0711@gmail.com>
2016-11-30 22:00:37 -0800

    [MOD] rooms.rb

--- controllers/rooms.rb
+++ controllers/rooms.rb
@@ -1,19 +1,36 @@
 # frozen_string_literal: true
 
 # TimeTravelerAPI web service
-class TimeTravelerAPI < Sinatra::Base   
-  get "/#{API_VER}/rent/:location/?" do
-    location = params[:location]
-    begin
-      rent = Airbnb::RentInfo.find(location: location)
+class TimeTravelerAPI < Sinatra::Base
+      location = params[:location]
+      begin
+        rent = Airbnb::RentInfo.find(location: location)
+
+      # Get each room's info
+      rent.infos.each do |room|
+        room_id = room.id
+        room_name = room.name
+        room_address = room.address
+        room_airbnb_link = room.airbnb_link
+        room_roomImg = room.roomImg
+        room_bed = room.bed
+        room_roomRank = room.roomRank
+      end
+
+      # Shown on the broswer
       content_type 'application/json'
       {
         location: rent.location,
         infos: rent.infos
+        infos: rooms_info
       }.to_json
 
       rescue
         halt 404, "Cannot find the location"
       end
+
+    rescue
+      halt 404, "Cannot find the location"
+    end
   end
-end
+end 
