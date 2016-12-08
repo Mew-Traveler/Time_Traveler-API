0bd819ec04bc4ae92ac2417fe2dacb26419dd10a
NTHUMan <q2221252@gmail.com>
2016-12-08 17:21:39 +0800

    start_refactor

--- controllers/addtarget.rb
+++ controllers/addtarget.rb
@@ -27,6 +27,7 @@ class TimeTravelerAPI < Sinatra::Base
     sites.to_json
   end
   
+  ####################################################################################################
    #find near by sites
   # get "/#{API_VER}/addtarget/findSite/:query/?" do
   #   query = params[:query]
@@ -79,22 +80,82 @@ class TimeTravelerAPI < Sinatra::Base
   #   end
   # end
 
+  ####################################################################################################
+
   #find the two sites desinations -> done
+  # get "/#{API_VER}/addtarget/countDistance/:origins/:destinations/?" do
+  #   origins = params[:origins]
+  #   destinations = params[:destinations]
+  #   params[:mode] = "driving"
+  #   mode = params[:mode]
+  #   result = Google::TrafficInfo.find(origins: origins, destinations: destinations, mode: mode)
+
+  #   content_type 'application/json'
+  #   {
+  #     anaDistance: result.anaDistance,
+  #     anaDuration: result.anaDuration
+  #   }.to_json
+  # end
+
   get "/#{API_VER}/addtarget/countDistance/:origins/:destinations/?" do
     origins = params[:origins]
     destinations = params[:destinations]
     params[:mode] = "driving"
     mode = params[:mode]
-    result = Google::TrafficInfo.find(origins: origins, destinations: destinations, mode: mode)
 
-    content_type 'application/json'
-    {
-    	anaDistance: result.anaDistance,
-    	anaDuration: result.anaDuration
-    }.to_json
+    result = CountDistance.call(params)
+    if result.success?
+      DistanceRepresenter.new(result.value).to_json
+    else
+      DistanceRepresenter.new(result.value).to_status_response
+    end
   end
 
+
+
   #find flight data -> done
+  # get "/#{API_VER}/addtarget/getFlightData/:project_day/:originPlace/:destinationPlace/?" do
+  #   params[:market] = "TW"
+  #   params[:currency] = "TWD"
+  #   params[:locale] = "en-GB"
+  #   params[:outboundPartialDate] = params[:project_day]
+
+  #   market = params[:market]
+  #   currency = params[:currency]
+  #   locale = params[:locale]
+  #   originPlace = params[:originPlace]
+  #   destinationPlace = params[:destinationPlace]
+  #   outboundPartialDate = params[:outboundPartialDate]
+
+  #   result = Skyscanner::FlightInfo.find(market: market, currency: currency, locale: locale, 
+  #     originPlace: originPlace, destinationPlace: destinationPlace, 
+  #     outboundPartialDate: outboundPartialDate)
+    
+  #   begin
+  #   infos = result.flightInfo
+  #   infos.map do |info|
+  #     if info["OutboundLeg"]["CarrierIds"].empty?
+  #       infos.delete(info)
+  #     end
+  #   end
+  #   flights = {
+  #     flights: infos.map do |info|
+  #       flight = {}
+  #       flight[:carrier] = info["OutboundLeg"]["CarrierIds"][0] if info["OutboundLeg"]["CarrierIds"][0]
+  #       flight[:originPlace] = info["OutboundLeg"]["OriginId"] if info["OutboundLeg"]["OriginId"]
+  #       flight[:destinationPlace] = info["OutboundLeg"]["DestinationId"] if info["OutboundLeg"]["DestinationId"]
+  #       {flight: flight}
+  #     end
+  #   }
+
+  #   content_type 'application/json'
+  #   flights.to_json
+  #   rescue
+  #     content_type 'text/plain'
+  #     halt 500, "No flight data with #{originPlace} and #{destinationPlace}"
+  #   end
+  # end
+
   get "/#{API_VER}/addtarget/getFlightData/:project_day/:originPlace/:destinationPlace/?" do
     params[:market] = "TW"
     params[:currency] = "TWD"
@@ -108,62 +169,54 @@ class TimeTravelerAPI < Sinatra::Base
     destinationPlace = params[:destinationPlace]
     outboundPartialDate = params[:outboundPartialDate]
 
-    result = Skyscanner::FlightInfo.find(market: market, currency: currency, locale: locale, 
-      originPlace: originPlace, destinationPlace: destinationPlace, 
-      outboundPartialDate: outboundPartialDate)
-    
-    begin
-    infos = result.flightInfo
-    infos.map do |info|
-      if info["OutboundLeg"]["CarrierIds"].empty?
-        infos.delete(info)
-      end
-    end
-    flights = {
-      flights: infos.map do |info|
-        flight = {}
-        flight[:carrier] = info["OutboundLeg"]["CarrierIds"][0] if info["OutboundLeg"]["CarrierIds"][0]
-        flight[:originPlace] = info["OutboundLeg"]["OriginId"] if info["OutboundLeg"]["OriginId"]
-        flight[:destinationPlace] = info["OutboundLeg"]["DestinationId"] if info["OutboundLeg"]["DestinationId"]
-        {flight: flight}
-      end
-    }
-
-    content_type 'application/json'
-    flights.to_json
-    rescue
-      content_type 'text/plain'
-      halt 500, "No flight data with #{originPlace} and #{destinationPlace}"
+    result = GetFlights.call(params)
+    if result.success?
+      FlightRepresenter.new(result.value).to_json
+    else
+      FlightRepresenter.new(result.value).to_status_response
     end
   end
 
   #add new sites into db
-  post "/#{API_VER}/addtarget/addSite2db/?" do #:project_id/:project_day/:site_name/:idx/:type/:start_time/:end_time
-  	body_params = JSON.parse request.body.read
-    newSite_dailyplans_id = body_params['dailyplans_id']
-    newSite_project_day = body_params['project_day']
-    newSite_idx = body_params['idx']
-    newSite_site_name = body_params['site_name']
-    newSite_type = body_params['type']
-    newSite_start_time = body_params['start_time']
-    newSite_end_time = body_params['end_time']
+  # post "/#{API_VER}/addtarget/addSite2db/?" do #:project_id/:project_day/:site_name/:idx/:type/:start_time/:end_time
+  #   body_params = JSON.parse request.body.read
+  #   newSite_dailyplans_id = body_params['dailyplans_id']
+  #   newSite_project_day = body_params['project_day']
+  #   newSite_idx = body_params['idx']
+  #   newSite_site_name = body_params['site_name']
+  #   newSite_type = body_params['type']
+  #   newSite_start_time = body_params['start_time']
+  #   newSite_end_time = body_params['end_time']
 
-    begin
-      if (Project.count == 0)
-        halt 422, "plan (addtarget: #{newSite_dailyplans_id})not found"
-      end
-    end
+  #   begin
+  #     if (Project.count == 0)
+  #       halt 422, "plan (addtarget: #{newSite_dailyplans_id})not found"
+  #     end
+  #   end
 
-    begin
-      newSite = Target.create(dailyplans_id: newSite_dailyplans_id, project_day: newSite_project_day, idx: newSite_idx, type: newSite_type, start_time: newSite_start_time, end_time: newSite_end_time)
-      content_type 'application/json'
-      newSite.to_json
-    rescue
-      content_type 'text/plain'
-      halt 500, "Cannot create site (id: #{newSite_site_name})"
+  #   begin
+  #     newSite = Target.create(dailyplans_id: newSite_dailyplans_id, project_day: newSite_project_day, idx: newSite_idx, type: newSite_type, start_time: newSite_start_time, end_time: newSite_end_time)
+  #     content_type 'application/json'
+  #     newSite.to_json
+  #   rescue
+  #     content_type 'text/plain'
+  #     halt 500, "Cannot create site (id: #{newSite_site_name})"
+  #   end
+  # end
+
+  post "/#{API_VER}/addtarget/addSite2db/?" do
+    result = AddSite2DB.call(request.body.read)
+
+    if result.success?
+      AddSiteRepresenter.new(result.value).to_json
+    else
+      AddSiteRepresenter.new(result.value).to_status_response
     end
   end
 
+  
+
+  #use for test, done
   post "/#{API_VER}/addtarget/addfortest/?" do
     puts Project.first.id.to_s
     Project.create(userId: 'test', projectName: 'test', dateStart: '2016-12-30', dateEnd: '2017-1-5', groupId: 'NULL')
